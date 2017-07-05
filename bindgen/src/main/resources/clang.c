c81c8b319937a27ae6a2b2979a12018036c7e91e
Jonas Fonseca <jonas.fonseca@mediative.com>
2017-07-05 15:47:05 -0400

    Clean up the libclang wrapper
    Removes Scala code and adds link to upstream ticket.

--- bindgen/src/main/resources/clang.c
+++ bindgen/src/main/resources/clang.c
@@ -1,11 +1,13 @@
 /**
- * Macro-based libclang wrapper from which both the C implementation and Scala
- * API are built.
+ * Macro-based libclang wrapper.
  *
- * Run the following command to see the Scala code:
- *
- *    $ clang -DSCALA -E bindgen/src/main/resources/clang.c | grep -v '^#'
+ * Required until Scala Native support passing structs as parameters:
+ * https://github.com/scala-native/scala-native/issues/195
  */
+
+#include <clang-c/Index.h>
+#include <stdlib.h>
+#include <string.h>
 
 // An incomplete list of enums. Not all of these are currently used.
 // Each enum value is exposed to Scala using a C method.
@@ -88,71 +90,6 @@
   STRING_(getCursorSpelling, CXCursor, *cursor) \
   STRING_(getTypeSpelling, CXType, *tpe)
 
-#ifdef SCALA
-package scala.scalanative
-package bindgen
-
-import scalanative.native._
-
-#define EXTERN_FUNCTION0(symbol, id, tpe) @name(#symbol) def id(): tpe = extern;
-#define EXTERN_FUNCTION1(symbol, id, tpe, argType, arg) \
-  @name(#symbol) def id(`arg`: argType): tpe = extern;
-
-#define DEFINE_ENUM(tpe, id) EXTERN_FUNCTION0(bindgen_clang_##id, id, tpe)
-
-#define PRIMITIVE_GETTER(tpe, name, argType, arg) \
-  EXTERN_FUNCTION1(bindgen_clang_##name, name, tpe, argType, arg)
-
-#define COPY_GETTER(tpe, name, argType, arg) \
-  EXTERN_FUNCTION1(bindgen_clang_##name, name, tpe, argType, arg)
-
-#define STRING_GETTER(name, argType, arg) \
-  EXTERN_FUNCTION1(bindgen_clang_##name, name, CString, argType, arg)
-
-@extern
-@link("clang")
-object ClangAPI {
-  type Data = Ptr[Byte]
-  type CXIndex = Ptr[Byte]
-  type CXCursor = Ptr[Byte]
-  type CXType = Ptr[Byte]
-  type CXTranslationUnit = Ptr[Byte]
-  type CXUnsavedFile = Ptr[Byte]
-  type Visitor = CFunctionPtr3[CXCursor, CXCursor, Data, UInt]
-
-  type CXCursorKind = UInt
-  type CXTypeKind = UInt
-  type CXTranslationUnit_Flags = UInt
-  type CXChildVisitResult = UInt
-  ENUM_INFO(DEFINE_ENUM)
-
-  GETTER_INFO(PRIMITIVE_GETTER, COPY_GETTER, STRING_GETTER)
-
-  @name("bindgen_clang_Cursor_getArgument")
-  def Cursor_getArgument(cursor: CXCursor, i: CInt): CXCursor = extern
-
-  @name("bindgen_clang_visitChildren")
-  def visitChildren(parent: CXCursor, visitor: Visitor, data: Data): UInt = extern
-
-  @name("clang_createIndex")
-  def createIndex(excludeDeclarationsFromPCH: CInt, displayDiagnostics: CInt): CXIndex = extern
-
-  @name("clang_disposeIndex")
-  def disposeIndex(index: CXIndex): Unit = extern
-
-  @name("clang_parseTranslationUnit")
-  def parseTranslationUnit(index: CXIndex, fileName: CString, argv: Ptr[CString], argc: CInt, unsavedFiles: CXUnsavedFile, numUnsavedFiles: CInt, options: UInt): CXTranslationUnit = extern
-
-  @name("clang_disposeTranslationUnit")
-  def disposeTranslationUnit(unit: CXTranslationUnit): Unit = extern
-}
-#endif
-
-#ifndef SCALA
-#include <clang-c/Index.h>
-#include <stdlib.h>
-#include <string.h>
-
 typedef enum CXCursorKind CXCursorKind;
 typedef long long CLongLong;
 typedef int CInt;
@@ -226,4 +163,3 @@ bindgen_clang_visitChildren(CXCursor *parent, bindgen_visitor visitor,  void *da
 
   return clang_visitChildren(*parent, bindgen_clang_visit, &ctx);
 }
-#endif
